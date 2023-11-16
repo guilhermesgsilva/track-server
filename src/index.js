516bb9eb6d0dda9232f47e7e0719ddc355f71733
Guilherme Silva <silva.gsg@gmail.com>
2023-11-16 17:47:44 +0000

    feat: MongoDB setup and connect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
+const dotenv = require("dotenv");
 const express = require("express");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+var username = encodeURIComponent(process.env.MONGODB_USERNAME);
+var password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+
+const mongoUri = `mongodb+srv://${username}:${password}@cluster0.vocmuhj.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoUri);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
 
 const app = express();
 
